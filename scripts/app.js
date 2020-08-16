a5f1f5a9499bdc6cac694a3fb09c034afd77c4c0
Leinier Perez <P_Leinier@yahoo.com>
2020-08-16 00:14:20 -0400

    Add button to make a new grid, add code to generate a random rgb value

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,12 @@
 let container = document.querySelector('.container');
+let div = document.querySelector('div');
+let body = document.querySelector('body');
+let btn = document.createElement('button');
+btn.style.width = '60px';
+btn.style.height = '20px';
+btn.style.alignSelf = 'flex-end';
+btn.innerHTML = 'hellp';
+body.appendChild(btn);
 
 function createGrid(x, y) {
 	let divW = 100 / x;
@@ -8,17 +16,29 @@ function createGrid(x, y) {
 			let div = document.createElement('div');
 			div.style.width = `${divW}%`;
 			div.style.height = `${divH}%`;
-			// div.style.border = '2px solid red';
-			div.style.display = 'inline-grid';
+			div.style.display = 'inline-block';
 			container.appendChild(div);
 		}
 	}
 }
 
-createGrid(32, 32);
+createGrid(16, 16);
 
-let div = document.querySelector('div');
 div.addEventListener('mouseover', (e) => {
-	let grid = e.target;
-	grid.style.background = 'gray';
+	e.target.style.background = 'black';
 });
+
+btn.onclick = () => {
+	while (container.firstChild) {
+		container.removeChild(container.firstChild);
+	}
+	let gridSize = window.prompt(`Enter Grid Size: `, '16');
+	createGrid(gridSize, gridSize);
+};
+
+function randRGB() {
+	let r = Math.floor(Math.random() * 256);
+	let g = Math.floor(Math.random() * 256);
+	let b = Math.floor(Math.random() * 256);
+	return `rgb(${r},${g},${b})`;
+}
